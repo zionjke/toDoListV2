01e8381447138186d17c7ce00a8531f86802919a
zionjke <vincere802@gmail.com>
2020-04-13 19:10:33 +0300

    add setState

--- src/App.js
+++ src/App.js
@@ -5,21 +5,39 @@ import TodoListTasks from "./components/TodoListTasks";
 import TodoListFooter from "./components/TodoListFooter";
 
 class App extends React.Component {
-    tasks = [
-        {title: "JS", isDone: true, priority: "priority:low"},
-        {title: "React",isDone: false, priority: "priority:medium"},
-        {title: "Angular",isDone: false, priority: "priority:high"}
-    ];
+    constructor(props) {
+        super(props);
+        setTimeout(()=>{
+            let newTask = {
+                title: "PHP",isDone: false, priority: "priority:low"
+            };
+                let newTasks = [...this.state.tasks,newTask];
+                this.setState({
+                    tasks:newTasks
+                });
 
-    filterValue = "All";
+        },2000);
+    }
+
+    state = {
+        tasks : [
+            {title: "JS", isDone: true, priority: "priority:low"},
+            {title: "React",isDone: false, priority: "priority:medium"},
+            {title: "Angular",isDone: false, priority: "priority:high"}
+        ],
+        filterValue : "All"
+    };
+
+
+
 
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
                     <TodoListHeader/>
-                    <TodoListTasks tasks={this.tasks}/>
-                    <TodoListFooter filterValue={this.filterValue}/>
+                    <TodoListTasks tasks={this.state.tasks}/>
+                    <TodoListFooter filterValue={this.state.filterValue}/>
                 </div>
             </div>
         );
